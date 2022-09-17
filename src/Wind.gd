cf0f5fbc17610deee016893abdf612e9d2fe5362
Tatiana Veratti <TatianaVeratti@gmail.com>
2022-09-17 18:23:14 -0500

    add wind rng, limit leaf angular velocity

--- src/Wind.gd
+++ src/Wind.gd
@@ -5,20 +5,15 @@ signal start(origin, direction)
 signal end()
 
 
-const DURATION:float = 1.5
-
-const DIRECTION_X_RANGE:float = 1.0
-const DIRECTION_Y_RANGE:float = 1.0
-const DIRECTION_Z_RANGE:float = 1.0
-
+const DIRECTION_RANGE:float = 1.0
 const ORIGIN_RANGE:float = 0.001
 
 
 static func get_random_direction() -> Vector3:
 	return Vector3(
-		randf_range(-DIRECTION_X_RANGE, DIRECTION_X_RANGE),
-		randf_range(0.1, DIRECTION_Y_RANGE),
-		randf_range(-DIRECTION_Z_RANGE, DIRECTION_Z_RANGE)
+		randf_range(-DIRECTION_RANGE, DIRECTION_RANGE),
+		randf_range(0.1, DIRECTION_RANGE),
+		randf_range(-DIRECTION_RANGE, DIRECTION_RANGE)
 	)
 
 
@@ -28,3 +23,11 @@ static func get_random_origin() -> Vector3:
 		randf_range(-1.0, -1.0),
 		randf_range(-ORIGIN_RANGE, ORIGIN_RANGE)
 	)
+
+
+static func get_random_wait() -> float:
+	return randf_range(5.0, 8.0)
+
+
+static func get_random_duration() -> float:
+	return randf_range(1.0, 3.0)
